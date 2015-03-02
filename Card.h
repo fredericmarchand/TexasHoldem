bc5ef78a056ba50601c7318b8f8fa3194e820877
fredericmarchand <fred.marchand.23@gmail.com>
2015-03-02 14:20:58 -0500

    updated card class

--- Card.h
+++ Card.h
@@ -11,24 +11,23 @@ typedef enum suits {
 } Suit;
 
 typedef enum card_values {
-        TWO = 2,
-        THREE = 3,
-        FOUR = 4,
-        FIVE = 5,
-        SIX = 6,
-        SEVEN = 7,
-        EIGHT = 8,
-        NINE = 9,
-        TEN = 10,
-        JACK = 11,
-        QUEEN = 12,
-        KING = 13,
-        Ace = 14,
+        TWO     = 2,
+        THREE   = 3,
+        FOUR    = 4,
+        FIVE    = 5,
+        SIX     = 6,
+        SEVEN   = 7,
+        EIGHT   = 8,
+        NINE    = 9,
+        TEN     = 10,
+        JACK    = 11,
+        QUEEN   = 12,
+        KING    = 13,
+        ACE     = 14,
 } CardValue;
 
 class Card 
 {
-
 public:
     Card(Suit suit, CardValue value);
     ~Card();
