d3ff29650e3bf79ef37aee499fca6d74a9bbaa79
Frederic Marchand <fred.marchand.23@gmail.com>
2015-04-17 14:17:20 -0400

    3 more funcs to impl

--- NodeState.cpp
+++ NodeState.cpp
@@ -1,19 +1,24 @@
 #include "NodeState.h"
 using namespace std;
 
-NodeState::NodeState()
+NodeState::NodeState(Game *game)
 {
-
+	game = new Game(game);
 }
 
 NodeState::NodeState(NodeState *state)
 {
-
+	game = new Game(state->getData());
 }
 
 NodeState::~NodeState()
 {
+	delete game;
+}
 
+Game* NodeState::getData()
+{
+	return game;
 }
 
 vector<Move>* NodeState::getMoves()
@@ -25,7 +30,7 @@ vector<Move>* NodeState::getMoves()
 
 void NodeState::doMove(Move move)
 {
-	
+
 }
 
 int NodeState::getResult(bool playerJustMoved)
--- NodeState.h
+++ NodeState.h
@@ -1,28 +1,30 @@
 #ifndef STATE_H
 #define STATE_H
 
-#include "Player.h"
+#include "Game.h"
 #include <vector>
 
 class NodeState
 {
 public:
-	NodeState();
+	NodeState(Game *game);
 	NodeState(NodeState *state);
 	~NodeState();
+	Game* getData();
 	vector<Move>* getMoves();
 	void doMove(Move move);
 	int getResult(bool pjm);
 
 private:
+	Game *game;
 	//Known information
-    Card *hand[2];
+    /*Card *hand[2];
     Card *flop[3];
     Card *turn;
     Card *river;
     int chips;
     int opponentChips;
-    int pot;
+    int pot;*/
 };
 
 #endif //STATE_H
