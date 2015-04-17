80df40e73df3864beac62a56bb426372b346c755
Frederic Marchand <fred.marchand.23@gmail.com>
2015-04-17 14:20:12 -0400

    Not using cilk after all

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 /*
- * TexasHoldemAI.cilk
+ * Texas Holdem AI
  * 
  * Frederic Marchand
  */
 
-#include <cilk/cilk.h>
-#include <cilktools/cilkview.h>
+//#include <cilk/cilk.h>
+//#include <cilktools/cilkview.h>
 #include <iostream>
 #include <string>
 using namespace std;
